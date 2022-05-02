2fbd946c0c59bf95abfa6b275d689b86e0196745
A1exEra <eramisant@yahoo.com>
2022-05-02 18:14:01 +0100

    feat:jumping

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 class Game {
-  constructor() {
+  constructor(parent) {
     const wrapper = document.createElement(`div`);
     const dyno = new Dyno(wrapper);
+    wrapper.className = `game`;
+    wrapper.tabIndex = 1;
+    wrapper.onkeydown = (event) => {
+      //   console.log(event.key);
+      if (event.code == `Space`) {
+        dyno.jump();
+      }
+    };
+    parent.append(wrapper);
+    const obstacle1 = new Obstacle(wrapper);
+    const render = (time) => {
+      requestAnimationFrame((timeStamp) => {
+        obstacle1.setPosition(obstacle1.position - timeStamp / 10000);
+        dyno.tick(timeStamp);
+        render(timeStamp);
+      });
+    };
+    render(Date.now());
   }
 }
 class Dyno {
@@ -9,6 +27,28 @@ class Dyno {
     const wrapper = document.createElement(`div`);
     wrapper.className = `dyno`;
     parent.append(wrapper);
+    this.element = wrapper;
+    this.position = 200;
+    this.element.style.top = this.position + `px`;
+  }
+  setPosition(position) {
+    this.position = position;
+    this.element.style.top = this.position + `px`;
+  }
+  tick(time) {
+    if (!this.lastTime) {
+      this.lastTime = time;
+    }
+    const delta = this.lastTime - time;
+    this.lastTime = time;
+    if (this.position < 200) {
+      this.setPosition(this.position - 1 * delta);
+    } else {
+      this.setPosition(200);
+    }
+  }
+  jump() {
+    this.setPosition(0);
   }
 }
 class Obstacle {
@@ -16,5 +56,14 @@ class Obstacle {
     const wrapper = document.createElement(`div`);
     wrapper.className = `obstacle`;
     parent.append(wrapper);
+    this.position = parent.clientWidth;
+    this.element = wrapper;
+    this.element.style.top = `200px`;
+  }
+  setPosition(position) {
+    // console.log(this.position);
+    this.position = position;
+    this.element.style.left = this.position + `px`;
   }
 }
+new Game(document.body);
